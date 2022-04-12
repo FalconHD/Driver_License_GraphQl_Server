0e0eb3d9cf2f5515ea4f7185485dd8179933cc52
FalconHD <youssbak.2015@gmail.com>
2022-04-12 10:43:23 +0000

    driver ready to go �

---
+++ src/schema/User/typeDefs.ts
@@ -0,0 +1,41 @@
+import { gql } from "apollo-server-express";
+
+// Construct a schema, using GraphQL schema language
+export const typeDefs = gql`
+  input UserInput {
+    name: String!
+    email: String!
+    phone: String!
+    cin: String!
+    score: String!
+    licenseType: String!
+  }
+
+  enum Role {
+    USER
+    ADMIN
+  }
+  enum LicenseType {
+    A
+    B
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    phone: String!
+    cin: String!
+    licenseType: LicenseType!
+    role: Role!
+    score: Int!
+  }
+
+  type Query {
+    Users: [User!]!
+  }
+
+  type Mutation {
+    submitData(input: UserInput): User
+  }
+`;
